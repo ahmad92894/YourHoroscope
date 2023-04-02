938d080fd81615d6390269e590caf815f20a5b5a
ahmad92894 <ahmad92894@gmail.com>
2023-04-02 16:26:23 -0500

    server running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,18 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
-//const { signToken } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    profiles: async () => {
+    user: async () => {
       return User.find();
-    }
+    },
 
-  }
-}
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+};
 
 
   module.exports = resolvers;
