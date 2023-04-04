8346b50662bfe423d2b9c1df6039275d300421d1
ahmad92894 <ahmad92894@gmail.com>
2023-04-04 13:34:33 -0500

    zodiacmain

--- client/src/pages/Zodiacmain.js
+++ client/src/pages/Zodiacmain.js
@@ -14,53 +14,53 @@ import virgo from '../img/virgo.png';
 
 const Zodiacmain = () => {
     return (
-<div class="container text-center">
-  <div class="row align-items-end">
-    <div class="col">
+<div className="container text-center">
+  <div className="row align-items-end">
+    <div className="col">
     <a href="client/src/pages/Aquarius.js"><img src={aqua} alt="aqua"/></a>
       Aqarius
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Aries.js"><img src={aries} alt="aries"/></a>
       Aries
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Cancer.js"><img src={cancer} alt="cancer"/></a>
         Cancer
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Capricorn.js"><img src={capricorn} alt="capricorn"/></a>    
         Capricorn
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Gemini.js"><img src={gemini} alt="gemini"/></a>    
         Gemini
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Leo.js"><img src={leo} alt="leo"/></a>
         Leo
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Libra.js"><img src={libra} alt="libra"/></a>    
         Libra
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Pisces.js"><img src={pisces} alt="pisces"/></a>
         Pisces
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Sagittarius.js"><img src={sagittarius} alt="sagittarius"/></a>"    
         Sagittarius
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Scorpio.js"><img src={scorpio} alt="scorpio"/></a>
         Scorpio
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Taurus.js"><img src={taurus} alt="taurus"/></a>
         Taurus
     </div>
-    <div class="col">
+    <div className="col">
     <a href="client/src/pages/Virgo.js"><img src={virgo} alt="virgo"/></a>
       Virgo
     </div>
