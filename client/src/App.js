e2b44fc265f4675de9bc65e6d080ec3dfc2117ef
kasthana112 <kanika.asthana@gmail.com>
2023-04-04 11:15:15 -0500

    added zodiac pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import NavBar from './componets/NavBar';
+import Footer from './componets/Footer';
 import './App.css';
+// import Aquarius from './pages/Aquarius';
 
 function App() {
   return (
@@ -23,8 +25,10 @@ function App() {
 
     <div1>
       <NavBar />
-      <Footer />
-
+      {/* <Aquarius /> */}
+      <Footer/>
+   
+    
     </div1>
   );
 }
