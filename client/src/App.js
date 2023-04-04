30335f122cf37136c308aa9f4c39658d8f3b2169
Jack Kelly <Carnificis@users.noreply.github.com>
2023-04-04 16:40:34 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import NavBar from './componets/NavBar';
 import Footer from './componets/Footer';
 import './App.css';
- import Aquarius from './pages/Aquarius';
+import Aquarius from './pages/Aquarius';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Route path="/Login" element={<login />} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
