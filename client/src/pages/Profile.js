d847e45bcbf05e2f772e087c5ef3199eec8b4496
ahmad92894 <ahmad92894@gmail.com>
2023-04-06 11:48:47 -0500

    oh lord

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -17,23 +17,23 @@
 //   });
 
 //   const user = data?.me || data?.user || {};
-//   // navigate to personal profile page if username is yours
-// //   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-// //     return <Navigate to="/me" />;
-// //   }
+  // navigate to personal profile page if username is yours
+//   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+//     return <Navigate to="/me" />;
+//   }
 
-// //   if (loading) {
-// //     return <div>Loading...</div>;
-// //   }
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
-// //   if (!user?.username) {
-// //     return (
-// //       <h4>
-// //         You need to be logged in to see this. Use the navigation links above to
-// //         sign up or log in!
-// //       </h4>
-// //     );
-// //   }
+//   if (!user?.username) {
+//     return (
+//       <h4>
+//         You need to be logged in to see this. Use the navigation links above to
+//         sign up or log in!
+//       </h4>
+//     );
+//   }
 
 //   return (
 //     <div>
