d8143f83fa07b766bd801701a66722b4efc4d437
Jack Kelly <Carnificis@users.noreply.github.com>
2023-04-06 18:46:54 -0500

    Login/Logout button fixed

--- client/src/components/NavBarUser.js
+++ client/src/components/NavBarUser.js
@@ -5,8 +5,6 @@ const NavBarUser = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   useEffect(() => {
-    // The setInterval function is not necessary here and can cause the component
-    // to re-render unnecessarily. Use the useState hook instead to update the user state.
     const userString = localStorage.getItem("user");
     setUser(JSON.parse(userString));
   }, []);
@@ -16,23 +14,19 @@ const NavBarUser = () => {
     setUser(null);
   };
 
+  const isLoggedIn = user ? true : false;
+
   return (
     <div className="navbar-nav ml-auto">
-      {/* Use a ternary operator to check whether the user is logged in or not */}
-      {user ? (
-        <>
-          <Link to="/" className="nav-item nav-link" onClick={logout}>
-            Logout
-          </Link>
-        </>
+      {isLoggedIn ? (
+        <Link to="/" className="nav-item nav-link" onClick={logout}>
+        </Link>
       ) : (
         <>
           <Link to="/login" className="nav-item nav-link">
-            Login
           </Link>
-          <span className="nav-item nav-link">|</span>
+          <span className="nav-item nav-link"></span>
           <Link to="/signup" className="nav-item nav-link">
-            Sign Up
           </Link>
         </>
       )}
