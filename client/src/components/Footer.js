534048d0fb1d893cdce734f9484dbe0285c0598c
ahmad92894 <ahmad92894@gmail.com>
2023-04-06 18:49:10 -0500

    filled in empty space

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>   <footer className='footer mt-auto py-3 bg-white text-black'>
+    
+    <footer className='footer mt-auto py-2 bg-white text-black'>
     <div className='container'>
+    <a href="url">Github</a>
+    
     </div>
-    
-    
   </footer>
-  </div>
+  
   )
 }
 
