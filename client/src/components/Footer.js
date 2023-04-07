80b6d754871c5f2612d11ad51ee20d3cd1955948
kasthana112 <kanika.asthana@gmail.com>
2023-04-06 20:56:57 -0500

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,12 +3,14 @@ import React from 'react'
 const Footer = () => {
   return (
     
-    <footer className='footer mt-auto py-8 bg-white text-black'>
-    <div className='container'>
-    <a href="https://github.com/ahmad92894/YourHoroscope">Github</a>
+
+   <footer>
+
+<a href="https://github.com/ahmad92894/YourHoroscope">github link</a>
+   </footer>
+  
     
-    </div>
-  </footer>
+
   
   )
 }
