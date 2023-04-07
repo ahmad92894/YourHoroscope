facfb8b153af4da87ee98ad4199fe7f54c2aae6c
Jack Kelly <Carnificis@users.noreply.github.com>
2023-04-07 13:26:47 -0500

    Merge

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from "react";
-import { Route, useNavigate } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { Route, useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children, isLoggedIn, ...rest }) => {
-  const navigate = useNavigate();
+// const ProtectedRoute = ({ children, isLoggedIn, ...rest }) => {
+//   const navigate = useNavigate();
 
-  const checkUserToken = () => {
-    const userToken = localStorage.getItem("user-token");
-    if (!userToken || userToken === "undefined") {
-      navigate("/auth/login");
-    }
-  };
+//   const checkUserToken = () => {
+//     const userToken = localStorage.getItem("user-token");
+//     if (!userToken || userToken === "undefined") {
+//       navigate("/auth/login");
+//     }
+//   };
 
-  useEffect(() => {
-    checkUserToken();
-  }, [isLoggedIn, navigate]);
+//   useEffect(() => {
+//     checkUserToken();
+//   }, [isLoggedIn, navigate]);
 
-  const ErrorMessage = () => {
-    const [errorMessage, setErrorMessage] = React.useState("");
-    const handleClick = () => {
-      setErrorMessage("Example error message!");
-    };
-    return (
-      <div className="App">
-        <button onClick={handleClick}>Show error message</button>
-        {errorMessage && <div className="error"> {errorMessage} </div>}
-      </div>
-    );
-  };
+//   const ErrorMessage = () => {
+//     const [errorMessage, setErrorMessage] = React.useState("");
+//     const handleClick = () => {
+//       setErrorMessage("Example error message!");
+//     };
+//     return (
+//       <div className="App">
+//         <button onClick={handleClick}>Show error message</button>
+//         {errorMessage && <div className="error"> {errorMessage} </div>}
+//       </div>
+//     );
+//   };
 
-  if (isLoggedIn) {
-    return <Route {...rest}>{children}</Route>;
-  } else {
-    return null;
-  }
-};
+//   if (isLoggedIn) {
+//     return <Route {...rest}>{children}</Route>;
+//   } else {
+//     return null;
+//   }
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
 
