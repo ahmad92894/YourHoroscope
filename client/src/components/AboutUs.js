ed3b65515b64dcb6df8163060b2276c49797c880
ahmad92894 <ahmad92894@gmail.com>
2023-04-10 13:34:09 -0500

    about me spacing

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -3,12 +3,12 @@ import React from 'react'
 const AboutUs = () => {
   return (
     
-   <div className="card-body">
+   <div className="card-body" >
      <h5 className="card-title">Personal GitHub Links</h5>
-     <a href="https://github.com/kasthana112" className="btn btn-primary">Kanika Asthana</a>
-     <a href="https://github.com/Tim-Smith1" className="btn btn-primary">Tim Smith</a>
-     <a href="https://github.com/Carnificis" className="btn btn-primary">Jack Kelly</a>
-     <a href="https://github.com/ahmad92894" className="btn btn-primary">Ahmad Bustani</a>
+     <a href="https://github.com/kasthana112" className="btn btn-primary"style={{ margin: "10px" }}>Kanika Asthana</a>
+     <a href="https://github.com/Tim-Smith1" className="btn btn-primary"style={{ margin: "10px" }}>Tim Smith</a>
+     <a href="https://github.com/Carnificis" className="btn btn-primary"style={{ margin: "10px" }}>Jack Kelly</a>
+     <a href="https://github.com/ahmad92894" className="btn btn-primary"style={{ margin: "10px" }}>Ahmad Bustani</a>
    </div>
  
  )
