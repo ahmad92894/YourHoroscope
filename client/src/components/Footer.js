0f3c7ec7508506dcda571ec9f36fc12fa21a5850
kasthana112 <kanika.asthana@gmail.com>
2023-04-10 13:18:35 -0500

    updated about us and footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     
    <div className="card-body">
-     <h5 className="card-title">Your Horoscope</h5>
-     <p className="card-text">Thank you for visiting are website.</p>
-     <a href="https://github.com/ahmad92894/YourHoroscope" className="btn btn-primary">github</a>
+     <h5 className="card-title">YourHoroscope</h5>
+     <p className="card-text">Thank you for visiting our website.</p>
+     <a href="https://github.com/ahmad92894/YourHoroscope" className="btn btn-primary">Github</a>
+     <Link to="/aboutus" className="btn btn-primary">About Us</Link>
    </div>
  
  )
