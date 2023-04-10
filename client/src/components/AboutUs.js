7797965691364225bbbf0eb5c3651d7ab11cee87
Tim Smith <meckaleck@hotmail.com>
2023-04-10 14:03:52 -0500

    fixed links

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -5,10 +5,10 @@ const AboutUs = () => {
     
    <div className="card-body" >
      <h5 className="card-title">Personal GitHub Links</h5>
-     <a href="https://github.com/kasthana112" className="btn btn-primary"style={{ margin: "10px" }}>Kanika Asthana</a>
-     <a href="https://github.com/Tim-Smith1" className="btn btn-primary"style={{ margin: "10px" }}>Tim Smith</a>
-     <a href="https://github.com/Carnificis" className="btn btn-primary"style={{ margin: "10px" }}>Jack Kelly</a>
-     <a href="https://github.com/ahmad92894" className="btn btn-primary"style={{ margin: "10px" }}>Ahmad Bustani</a>
+     <a href="https://github.com/kasthana112" target="_blank" className="btn btn-primary"style={{ margin: "10px" }}>Kanika Asthana</a>
+     <a href="https://github.com/Tim-Smith1" target="_blank" className="btn btn-primary"style={{ margin: "10px" }}>Tim Smith</a>
+     <a href="https://github.com/Carnificis" target="_blank" className="btn btn-primary"style={{ margin: "10px" }}>Jack Kelly</a>
+     <a href="https://github.com/ahmad92894" target="_blank" className="btn btn-primary"style={{ margin: "10px" }}>Ahmad Bustami</a>
    </div>
  
  )
